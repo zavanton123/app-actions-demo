c02614e6d64c1b53bdd90759fbc1e459c46c67aa
zavanton <zavanton123@gmail.com>
2021-04-06 16:47:42 +0300

    deep link to card list added

--- app/src/main/java/com/zavanton/appactionsdemo/MainActivity.kt
+++ app/src/main/java/com/zavanton/appactionsdemo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.zavanton.appactionsdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("zavanton", "zavanton - intent.action: ${intent.action}")
+        Log.d("zavanton", "zavanton - intent.data: ${intent.data}")
     }
 }
