f354f50d26f964fa2490308b66c68b925b10bd76
zavanton <zavanton123@gmail.com>
2021-04-09 11:52:50 +0300

    cards, accounts, home features

--- app/src/main/java/com/zavanton/appactionsdemo/Constants.kt
+++ app/src/main/java/com/zavanton/appactionsdemo/Constants.kt
@@ -3,4 +3,5 @@ package com.zavanton.appactionsdemo
 object Deeplink {
     const val CARD_LIST = "/card-list"
     const val ACCOUNT_LIST = "/account-list"
+    const val SEARCH = "/search"
 }
--- app/src/main/java/com/zavanton/appactionsdemo/MainActivity.kt
+++ app/src/main/java/com/zavanton/appactionsdemo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.zavanton.appactionsdemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.zavanton.appactionsdemo.features.AccountListFragment
 import com.zavanton.appactionsdemo.features.CardListFragment
 import com.zavanton.appactionsdemo.features.HomeFragment
+import com.zavanton.appactionsdemo.features.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,16 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (intent.action == Intent.ACTION_VIEW) {
+            Log.d("zavanton", "zavanton - intent.data: ${intent.data}")
+            Log.d("zavanton", "zavanton - intent.data?.path: ${intent.data?.path}")
+
             val fragment = when {
                 intent.data?.path == Deeplink.CARD_LIST -> CardListFragment.newInstance()
                 intent.data?.path == Deeplink.ACCOUNT_LIST -> AccountListFragment.newInstance()
+                intent.data?.path == Deeplink.SEARCH -> SearchFragment.newInstance()
                 else -> HomeFragment.newInstance()
             }
+
             if (supportFragmentManager.findFragmentById(R.id.fragment_container) == null) {
                 supportFragmentManager.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit()
             }
+        } else {
+            Log.d("zavanton", "zavanton - not deep link")
         }
     }
 }
